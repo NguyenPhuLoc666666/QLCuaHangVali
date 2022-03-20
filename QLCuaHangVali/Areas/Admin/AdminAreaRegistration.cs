ff11a3d620e5791901b2b6feb6c256b4cbf777ea
Quân Anh <01642027120q@gmail.com>
2022-03-20 11:56:56 +0700

    Update giao diện cơ bản - Tạo trang Admin cơ bản By Anh Quân

---
+++ QLCuaHangVali/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace QLCuaHangVali.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { Controller="Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+            //Dashboard: bảng điều khiển - trang chủ
+        }
+    }
+}
