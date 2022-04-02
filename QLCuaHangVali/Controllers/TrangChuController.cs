d197215f4922db6ee7462330f38f84ca53fa4bc7
anhtuana2k422001 <82400786+anhtuana2k422001@users.noreply.github.com>
2022-04-02 23:49:28 +0700

    Tuấn cập nhật giao diện

--- QLCuaHangVali/Controllers/TrangChuController.cs
+++ QLCuaHangVali/Controllers/TrangChuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLCuaHangVali.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,28 @@ namespace QLCuaHangVali.Controllers
     public class TrangChuController : Controller
     {
         // GET: TrangChu
+        ValiDBDataContext db = new ValiDBDataContext();
+        private List<VALI> Vailimoi(int count)
+        {
+            return db.VALIs.OrderByDescending(a => a.ngaytao).Take(count).ToList();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var sachmoi = Vailimoi(3);
+            return View(sachmoi);
+        }
+
+        public ActionResult DanhMuc()
+        {
+            var Danhmuc = from dm in db.DANHMUCVALIs select dm;
+            return PartialView(Danhmuc);
+        }
+
+        public ActionResult ThuongHieu()
+        {
+            var ThuongHieu = from dm in db.THUONGHIEUs select dm;
+            return PartialView(ThuongHieu);
         }
     }
 }
